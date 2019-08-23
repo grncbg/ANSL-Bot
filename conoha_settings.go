b25a460361b45710e297d6ae8ea6382503f2b1d8
grncbg <grncbg@gmail.com>
2019-08-23 17:17:22 +0900

    feat: import conoha settings from env
    環境変数からConoHaの設定を読み込む

---
+++ conoha_settings.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/kelseyhightower/envconfig"
+
+// ConohaSettings has ConoHa API settings
+type ConohaSettings struct {
+	Endpoints struct {
+		AccountServiceURL       string `split_words:"true"`
+		ComputeServiceURL       string `split_words:"true"`
+		VolumeServiceURL        string `split_words:"true"`
+		DatabaseServiceURL      string `split_words:"true"`
+		ImageServiceURL         string `split_words:"true"`
+		DNSServiceURL           string `split_words:"true"`
+		ObjectStorageServiceURL string `split_words:"true"`
+		MailServiceURL          string `split_words:"true"`
+		IdentityServiceURL      string `split_words:"true"`
+		NetworkServiceURL       string `split_words:"true"`
+	}
+	Username string `required:"true" split_words:"true"`
+	Password string `required:"true" split_words:"true"`
+	TenantID string `required:"true" split_words:"true"`
+	Token    string
+}
+
+// NewConohaSettings is constructor of ConohaSettings
+func NewConohaSettings() (*ConohaSettings, error) {
+	var conohaSettings ConohaSettings
+	err := envconfig.Process("conoha", &conohaSettings)
+	return &conohaSettings, err
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/grncbg/ANSL-Bot/bot
 
 go 1.12
+
+require github.com/kelseyhightower/envconfig v1.4.0
